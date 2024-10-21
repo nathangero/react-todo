26d86784b5e93a0d1614e719addce5ee99281258
Nathan Geronimo <nathangero@yahoo.com>
2024-10-21 15:20:53 -0700

    implement locally updating todo items

--- client/src/components/TodoList.tsx
+++ client/src/components/TodoList.tsx
@@ -1,10 +1,24 @@
-import { useEffect } from "react";
+import { ChangeEvent, useEffect } from "react";
 
 export default function TodoList({ todos, setTodos }: TodoListProps) {
   const INDEX_TODO_NAME = 0;
   const INDEX_TODO_COMPLETION = 1;
 
+  const onClickCheckbox = async (event: ChangeEvent<HTMLInputElement>, timestamp: number) => {
+    const completed = event.target.checked;
+    console.log(completed);
+
+    setTodos({
+      ...todos,
+      [timestamp]: {
+        ...todos[timestamp],
+        [INDEX_TODO_COMPLETION]: !!completed
+      }
+    });
+  }
+
   useEffect(() => {
+    // Get all the todos upon page refresh
     const getAllTodos = async () => {
       try {
         const response = await fetch("http://localhost:3001/allTodos", {
@@ -39,11 +53,18 @@ export default function TodoList({ todos, setTodos }: TodoListProps) {
         <hr className="flex m-auto max-w-xl" />
         <>
           {Object.keys(todos).map((key) => {
-            const todo = todos[Number(key)];
+            const todo = todos[key];
             return (
               <li key={key} className="flex items-center font-bold text-lg mx-auto max-w-xl border-b border-gray-300 pb-4">
                 <p className="flex-1 font-semibold text-3xl">{todo[INDEX_TODO_NAME]}</p>
-                <input type="checkbox" className="flex-1 form-checkbox h-6 w-6" checked={todo[INDEX_TODO_COMPLETION]} disabled />
+                <div className="flex-1">
+                  <input
+                    type="checkbox"
+                    className="form-checkbox h-6 w-6"
+                    onChange={(event) => onClickCheckbox(event, key)}
+                    checked={todo[INDEX_TODO_COMPLETION]}
+                  />
+                </div>
               </li>
             )
           })
