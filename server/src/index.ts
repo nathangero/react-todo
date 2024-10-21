813e6e29aecd0134b9c5e961a287a9b911e0d8b9
Nathan Geronimo <nathangero@yahoo.com>
2024-10-21 14:15:34 -0700

    implement all crud operations

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,7 +5,9 @@ import cors from "cors";
 const app = express();
 const PORT = 3001;
 
-const db: { [key: number]: [string, boolean] } = {};
+const DB: { [key: number]: [string, boolean] } = {};
+const INDEX_TODO_NAME = 0;
+const INDEX_TODO_COMPLETION = 1;
 
 app.use(cors());
 app.use(express.json());
@@ -15,15 +17,45 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Welcome to the server!");
 });
 
+app.get("/allTodos", (req: Request, res: Response) => {
+  console.log("get all todos");
+
+  res.send(JSON.stringify(DB));
+});
+
 app.put("/todo", (req: Request, res: Response) => {
   console.log("put todo");
 
   const todo = req.body.todo;
-  db[Date.now()] = [todo, false];
+  const time = Date.now();
+  DB[time] = [todo, false];
 
-  console.log(db);
+  console.log(DB);
 
-  res.sendStatus(201);
+  res.status(200).json([time, DB[time]]);
+});
+
+app.patch("/todo", (req: Request, res: Response) => {
+  console.log("update todo");
+
+  const timestamp = req.body.timestamp;
+  const todoName = req.body.todoName;
+  const completed = req.body.completed;
+
+  if (todoName) DB[timestamp][INDEX_TODO_NAME] = todoName; // Edit the todo name if it exists
+  DB[timestamp][INDEX_TODO_COMPLETION] = completed;
+
+  res.status(200).json(DB[timestamp]);
+});
+
+app.delete("/:timestamp", (req: Request, res: Response) => {
+  console.log("delete todo");
+
+  const timestamp = parseInt(req.params.timestamp);
+  console.log("delete todo at timestamp:", timestamp);
+
+  delete DB[timestamp];
+  res.status(200).json(DB);
 });
 
 app.listen(PORT, () => {
