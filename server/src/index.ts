b588fec516e97db742ab92926de8643501e1dd91
Nathan Geronimo <nathangero@yahoo.com>
2024-10-21 13:55:22 -0700

    implement adding a new todo to the server

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,11 +4,24 @@ import { Request, Response } from "express";
 const app = express();
 const PORT = 3001;
 
+const db: { [key: number]: [string, boolean] } = {};
+
 app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
   console.log("server ping");
   res.send("Welcome to the server!");
+});
+
+app.put("/todo", (req: Request, res: Response) => {
+  console.log("put todo");
+
+  const todo = req.body.todo;
+  db[Date.now()] = [todo, false];
+
+  console.log(db);
+
+  res.sendStatus(201);
 });
 
 app.listen(PORT, () => {
